9d1c6f84949bd5bf62169f75723b41e7e6d99e6d
robertliwpe <51431603+robertliwpe@users.noreply.github.com>
2022-02-22 12:41:45 +1000

    Create disksizebrowser.php

---
+++ disksizebrowser.php
@@ -0,0 +1,49 @@
+<?php
+    $units = explode(' ', 'B KB MB GB TB PB');
+    $currentfolder = getcwd();
+    $disk_used = foldersize($currentfolder);
+
+
+    echo("<html><body>");
+    echo('diskspace used: ' . format_size($disk_used) . '<br>');
+    echo("</body></html>");
+
+
+function foldersize($path) {
+    $total_size = 0;
+    $files = scandir($path);
+    $cleanPath = rtrim($path, '/'). '/';
+
+    foreach($files as $t) {
+        if ($t<>"." && $t<>"..") {
+            $currentFile = $cleanPath . $t;
+            if (is_dir($currentFile)) {
+                $size = foldersize($currentFile);
+                $total_size += $size;
+            }
+            else {
+                $size = filesize($currentFile);
+                $total_size += $size;
+            }
+        }
+    }
+
+    return $total_size;
+}
+
+
+function format_size($size) {
+    global $units;
+
+    $mod = 1024;
+
+    for ($i = 0; $size > $mod; $i++) {
+        $size /= $mod;
+    }
+
+    $endIndex = strpos($size, ".")+3;
+
+    return substr( $size, 0, $endIndex).' '.$units[$i];
+}
+
+?>
